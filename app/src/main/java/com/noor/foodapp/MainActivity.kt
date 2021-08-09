59852a690daa8cadef35b57e7833b08b7a671c1c
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-09 12:36:40 +0300

    Recipies Fragment XML View created.

--- app/src/main/java/com/noor/foodapp/MainActivity.kt
+++ app/src/main/java/com/noor/foodapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-,
+        
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.navBar.setupWithNavController(
