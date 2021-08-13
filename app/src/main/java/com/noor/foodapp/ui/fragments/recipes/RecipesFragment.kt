a7e8c4be4977918de2900c46aea7736868af76e1
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-13 12:23:04 +0300

    Recipes Fragment - Connect the Dots Create RecipesViewModel.kt

--- app/src/main/java/com/noor/foodapp/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/noor/foodapp/ui/fragments/recipes/RecipesFragment.kt
@@ -5,23 +5,81 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.noor.foodapp.viewmodels.MainViewModel
+import com.noor.foodapp.adapters.RecipesAdapter
 import com.noor.foodapp.databinding.FragmentRecipesBinding
+import com.noor.foodapp.util.Constants.Companion.API_KEY
+import com.noor.foodapp.util.NetworkResult
+import com.noor.foodapp.viewmodels.RecipesViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
     private lateinit var binding: FragmentRecipesBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRecipesBinding.inflate(layoutInflater, container, false)
+
         // Inflate the layout for this fragment
 
-        binding.recyclerView.showShimmer()
+        setupRecyclerView()
+        requestAPIData()
 
         return binding.root
+    }
+
+    private fun requestAPIData() {
+        viewModel.getRecipes(recipesViewModel.applyQueries())
+        viewModel.recipesResponse.observe(viewLifecycleOwner, {
+            when (it) {
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    it.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        it.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.adapter = mAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        binding.recyclerView.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        binding.recyclerView.hideShimmer()
     }
 
 }
--- app/src/main/java/com/noor/foodapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/noor/foodapp/viewmodels/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.noor.foodapp
+package com.noor.foodapp.viewmodels
 
 import android.app.Application
 import android.content.Context
