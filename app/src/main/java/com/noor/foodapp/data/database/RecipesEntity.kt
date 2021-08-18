077760287a18b900ce2cff145ca60dc4c047ca97
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-18 11:00:18 +0300

    Create Database and Recipes Table

---
+++ app/src/main/java/com/noor/foodapp/data/database/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.noor.foodapp.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.noor.foodapp.models.FoodRecipe
+import com.noor.foodapp.util.Constants.Companion.RECIPES_TABLE
+
+@Entity( tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 0
+
+}
--- app/src/main/java/com/noor/foodapp/util/Constants.kt
+++ app/src/main/java/com/noor/foodapp/util/Constants.kt
@@ -8,13 +8,17 @@ class Constants {
         const val API_KEY = "ffc510c8409140bfa6bdf5da20f0edda"
         const val BASE_URL = "https://api.spoonacular.com"
 
-        //API Query Keys
+        // API Query Keys
         const val QUERY_NUMBER = "number"
         const val QUERY_API_KEY = "apiKey"
         const val QUERY_TYPE = "type"
         const val QUERY_DIET = "diet"
         const val QUERY_ADD_RECIPE_INFORMATION= "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+        // Room Database
+        const val DATABASE_NAME = "recipes_database"
+        const val RECIPES_TABLE = "recipes_table"
     }
 
 }
