35f6803ee75bae5f2d4c7b92efc5cb7d96331b81
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-18 14:50:40 +0300

    Design RecipesBottomSheet

---
+++ app/src/main/java/com/noor/foodapp/ui/fragments/recipes/bootomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,20 @@
+package com.noor.foodapp.ui.fragments.recipes.bootomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.noor.foodapp.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+
+}
