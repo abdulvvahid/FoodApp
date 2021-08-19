b060d217d0473285549796819a7acb9df27f18ac
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-19 14:06:26 +0300

    Design activity_details.xml

---
+++ app/src/main/java/com/noor/foodapp/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.noor.foodapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noor.foodapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
