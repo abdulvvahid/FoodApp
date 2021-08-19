50a2cd189a9fc817a7821aa88d8524a95958c418
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-19 12:50:08 +0300

    FoodRecipesAPI - searchRecipes() RemoteDataSource - searchRecipes()

--- app/src/main/java/com/noor/foodapp/data/RemoteDataSource.kt
+++ app/src/main/java/com/noor/foodapp/data/RemoteDataSource.kt
@@ -12,4 +12,9 @@ class RemoteDataSource @Inject constructor(
     suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
         return foodRecipesAPI.getRecipes(queries)
     }
+
+    suspend fun searchRecipes(searchQuery: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesAPI.searchRecipes(searchQuery)
+    }
+    
 }
