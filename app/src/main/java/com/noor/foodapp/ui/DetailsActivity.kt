e78901fb4884000d61d9aefb2becede3c4bffc00
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-19 18:52:05 +0300

    Create PagerAdapter Class

--- app/src/main/java/com/noor/foodapp/ui/DetailsActivity.kt
+++ app/src/main/java/com/noor/foodapp/ui/DetailsActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.noor.foodapp.R
+import com.noor.foodapp.adapters.PagerAdapter
 import com.noor.foodapp.databinding.ActivityDetailsBinding
+import com.noor.foodapp.ui.fragments.ingredients.IngredientsFragment
+import com.noor.foodapp.ui.fragments.instructions.InstructionsFragment
+import com.noor.foodapp.ui.fragments.overview.OverviewFragment
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private val args by navArgs<DetailsActivityArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +27,28 @@ class DetailsActivity : AppCompatActivity() {
         binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val adapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+
+        binding.viewPager.adapter = adapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
