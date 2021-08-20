430186a33a0b9208f66790913c1234dd27a65df7
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-20 12:36:27 +0300

    Modify Recipes Diff Util

--- app/src/main/java/com/noor/foodapp/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/noor/foodapp/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.noor.foodapp.util
 import androidx.recyclerview.widget.DiffUtil
 import com.noor.foodapp.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ): DiffUtil.Callback() {
 
     override fun getOldListSize(): Int = oldList.size
