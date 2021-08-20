e13eb1c26d5349cefea8544e1ef7d0f2f007f4ee
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-20 16:42:55 +0300

    Modify RecipesDatabase

--- app/src/main/java/com/noor/foodapp/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/noor/foodapp/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.noor.foodapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.noor.foodapp.data.database.entities.FavoritesEntity
 import com.noor.foodapp.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
