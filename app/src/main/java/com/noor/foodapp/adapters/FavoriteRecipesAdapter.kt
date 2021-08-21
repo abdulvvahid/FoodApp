a6c4da2015a1186e3cd0de1f5030702d648b84f9
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-21 10:48:58 +0300

    Apply Contextual Action Mode Style

--- app/src/main/java/com/noor/foodapp/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/noor/foodapp/adapters/FavoriteRecipesAdapter.kt
@@ -2,6 +2,7 @@ package com.noor.foodapp.adapters
 
 import android.view.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
@@ -59,15 +60,10 @@ class FavoriteRecipesAdapter(
 
     override fun getItemCount(): Int = favoriteRecipes.size
 
-    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
-        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
-        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
-        favoriteRecipes = newFavoriteRecipes
-        diffUtilResult.dispatchUpdatesTo(this)
-    }
 
     override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
         actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        applyStatusBarColor(R.color.contextualStatusBarColor)
         return true
     }
 
@@ -80,6 +76,18 @@ class FavoriteRecipesAdapter(
     }
 
     override fun onDestroyActionMode(actionMode: ActionMode?) {
-
+        applyStatusBarColor(R.color.statusBarColor)
     }
+
+    private fun applyStatusBarColor(color: Int) {
+        requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
+    }
+
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
+        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
 }
